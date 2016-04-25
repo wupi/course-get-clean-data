fe74ba276663d5e274ba5d5e93e24f457865774b
woo2004cn <woo2004cn@gmail.com>
2016-04-24 17:50:47 -0700

    no message

--- notes of R command.R
+++ notes of R command.R
@@ -1,22 +1,19 @@
 # note of frequently used commands
 
-
 #download and read xls file
-
 download.file(fileurl, destfile = "./dataclean_w1/q1.csv", method = 'curl')
- q3 <- read.xlsx("./dataclean_w1/q3.xlsx", sheetIndex = 1, header = T, rowIndex = 18:23, colIndex = 7:15)
+q3 <- read.xlsx("./dataclean_w1/q3.xlsx", sheetIndex = 1, header = T, rowIndex = 18:23, colIndex = 7:15)
 
- 
- #download and read xml files
- #transform to data frame
- library(XML)
- fileurl <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml'
- doc <- xmlTreeParse(fileurl, useInternalNodes = T)
- rootNode <- xmlRoot(doc)
- 
- # To extract the XML-values from the document, use xmlSApply:
- docvalue <- xmlSApply(xmltop, function(x) xmlSApply(x, xmlValue))
- 
- # Finally, get the data in a data-frame and have a look at the first rows and columns
- plantcat_df <- data.frame(t(docvalue),row.names=NULL)
- 
+
+#download and read xml files
+#transform to data frame
+#caution: pay attention to which level of xml would you like to read
+library(XML)
+fileurl<- 'https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml'
+doc <- xmlTreeParse(fileurl, useInternalNodes = T)
+rootNode <- xmlRoot(doc)
+doc_df <- xmlToDataFrame(doc)
+
+
+# test performance
+system.time(DT[,mean(pwgtp15),by=SEX])
